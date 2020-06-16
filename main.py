50a54d706db043780a6b25a7d76d6b10cbd52dd5
unknown <pitoab@wp.pl>
2020-06-16 16:19:21 +0200

    finish add button

--- main.py
+++ main.py
@@ -2,28 +2,29 @@ import function
 from tkinter import *
 import stringSpacePL
 
+
 def writeAllAccount():
+    accountList.delete(0, END)#czyści liste
     for accoun in range(function.amountOfAccont()):
         # Wczytuje tylko nazwe konta bo w lini zapisana jest nazwa i hasło
-        textbox.insert(INSERT, function.readAccount(accoun).split(' ')[0]+"\n")
-
-
-###################################################
-
+        accountList.insert(END, function.readAccount(accoun).split(' ')[0]+"")
 
 def writeAccountToList():
     f = open("account.txt", "a")
-    f.write(login.get())
+    f.write(login.get()+" ")
+    f.write(password.get()+"\n")
     f.close()
-    print("aaa")
+    writeAllAccount()
 
+###################################################
 
 
 
 root = Tk()
 root.title(stringSpacePL.dailyBonus)
 
-textbox = Text(root, width=50, height=10)
+#textbox = Text(root, width=50, height=10)
+accountList = Listbox(root, width=50, height=10)
 loginLb = Label(root, text=stringSpacePL.login)
 passwordLb = Label(root, text=stringSpacePL.password)
 login = Entry(root, width=35, borderwidth=2)
@@ -34,7 +35,8 @@ addAccount = Button(root, text=stringSpacePL.add, command=writeAccountToList, wi
 writeAllAccount()#on textbox
 
 
-textbox.grid(rowspan=10, column=0, padx=20, pady=20)
+#textbox.grid(rowspan=10, column=0, padx=20, pady=20)
+accountList.grid(rowspan=10, column=0, padx=20, pady=20)
 loginLb.grid(row=0, column=1, padx=0, pady=(20, 0))
 login.grid(row=0, column=2, padx=0, pady=(20, 0))
 passwordLb.grid(row=1, column=1, padx=10, pady=(0, 0))
